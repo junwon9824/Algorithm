325110c1f6bade5e5192a5e310b489c4142e7eca
junwon9824 <42957005+junwon9824@users.noreply.github.com>
2023-07-08 13:26:54 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,12 @@
-SELECT ANIMAL_INS.ANIMAL_ID, ANIMAL_INS.NAME
-FROM ANIMAL_INS
-LEFT JOIN ANIMAL_OUTS
-ON ANIMAL_INS.ANIMAL_ID = ANIMAL_OUTS.ANIMAL_ID
-WHERE ANIMAL_INS.DATETIME > ANIMAL_OUTS.DATETIME
-ORDER BY ANIMAL_INS.DATETIME
+-- 코드를 입력하세요
+# SELECT *
+SELECT ai.ANIMAL_ID , ai.NAME
+
+
+from ANIMAL_INS ai   join ANIMAL_OUTS ao on ai.animal_id=ao.animal_id
+
+
+# where datediff(ai.datetime,ao.datetime)>0
+WHERE ai.DATETIME > ao.datetime
+
+order by ai.datetime
