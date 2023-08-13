f6c01b563272b0a6ffa95a94aba4333362186d41
junwon9824 <42957005+junwon9824@users.noreply.github.com>
2023-08-13 12:51:15 +0900

    [Silver III] Title: 모든 순열, Time: 784 ms, Memory: 33504 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -2,38 +2,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	static boolean[] visit;
-	static int[] res;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		res = new int[n];
-		visit = new boolean[n];
-		solve(n, 0);
+		int[] permutation = new int[n];
+		//System.out.println("in");
+		solve(n, 0, 0, permutation);
+
 	}
 
-	static void solve(int n, int depth) {
+	static void solve(int n, int depth, int mask, int[] permutation) {
 
 		if (depth == n) {
-			for (int i : res)
-				System.out.print(i + " ");
-			System.out.println();
+//			System.out.println("fin");
+			StringBuilder sb = new StringBuilder();
+//			System.out.println("ar" + Arrays.toString(permutation));
+			for (int i : permutation)
+				sb.append(i).append(" ");
+//				System.out.print(i + " ");
+//			System.out.println();
+			//sb.append('\n');
+			System.out.println(sb.toString());
 			return;
 		}
 
-		for (int i = 0; i < n; i++) {
-			if (visit[i])
+		for (int i = 1; i <= n; i++) {
+
+			if ((mask & (1 << i)) != 0) {
 				continue;
-
-			visit[i] = true;
-			res[depth] = i + 1;
-			solve(n, depth + 1);
-			visit[i] = false;
+			}
+			permutation[depth] = i;
+//			System.out.println("perm" + permutation[depth]);
+			solve(n, depth + 1, mask | (1 << i), permutation);
 		}
-
 	}
 }
