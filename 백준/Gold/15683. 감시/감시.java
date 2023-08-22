acc881ff3ef2d86e0928868b2879b967193f5c6f
junwon9824 <42957005+junwon9824@users.noreply.github.com>
2023-08-23 02:16:54 +0900

    [Gold IV] Title: 감시, Time: 644 ms, Memory: 301748 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,178 +1,249 @@
  
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import javax.print.attribute.standard.Sides;
 
 public class Main  {
-	public static int N, M;
-	public static int[][] map;
-	public static int[][] copyMap;
-	public static int[] output;
-	public static ArrayList<CCTV> cctvList;
-	public static int[] dx = { -1, 0, 1, 0 }; // 상 우 하 좌 시계방향 순서
-	public static int[] dy = { 0, 1, 0, -1 };
-	public static int answer = Integer.MAX_VALUE;
+	static char arr[][];
+	static int[] dr = { -1, 0, 1, 0 }; // 상좌하우
+	static int[] dc = { 0, -1, 0, 1 };
+	static int res = Integer.MAX_VALUE;
+	static int N, L, M;
+	// 상좌하우
+	static int cctvsize = 0;
+	static int cctv[][] = { { 0, 1, 0, 0 }, { 0, 1, 0, 1 }, { 1, 1, 0, 0 }, { 1, 1, 0, 1 }, { 1, 1, 1, 1 } };
+	static int[][] visit;
+	static int[] output;
+	static ArrayList<int[]> list;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		map = new int[N][M];
-		cctvList = new ArrayList<>();
+	public static void main(String args[]) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new char[N][M];
+		visit = new int[N][M];
+		list = new ArrayList<>();
 
 		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				map[i][j] = sc.nextInt();
-
-				if (map[i][j] != 0 && map[i][j] != 6) {
-					cctvList.add(new CCTV(map[i][j], i, j));
+				arr[i][j] = st.nextToken().charAt(0);
+				if (arr[i][j] >= '1' && arr[i][j] <= '5') {
+					cctvsize++;
+					list.add(new int[] { i, j });
 				}
 			}
+
 		}
 
-		output = new int[cctvList.size()]; // 순열을 담을 배열
-		permutation(0, cctvList.size());
-
-		System.out.println(answer);
+		for (int i = 0; i < N; i++) {
+			// System.out.println(Arrays.toString(arr[i]));
+		}
+		// 조합
+		output = new int[cctvsize];
+		permu(0);
+		System.out.println(res);
 	}
 
-	// DFS로 상하좌우 4방향 중에서 cctv의 총 개수, r만큼을 순서대로 뽑는 순열을 구함
-	public static void permutation(int depth, int r) {
-		if (depth == r) {
-			// Copy original 'map' array
-			copyMap = new int[N][M];
-			for (int i = 0; i < map.length; i++) {
-				System.arraycopy(map[i], 0, copyMap[i], 0, map[i].length);
+	static void permu(int depth) { // 중복조합...
+		if (depth == cctvsize) {
+			int[][] cctvcopy = new int[5][4];
+			char[][] arrcopy = new char[N][M];
+
+			for (int s = 0; s < 5; s++)
+				System.arraycopy(cctv[s], 0, cctvcopy[s], 0, cctv[0].length);
+
+			for (int s = 0; s < N; s++)
+				System.arraycopy(arr[s], 0, arrcopy[s], 0, M);
+
+			for (int i = 0; i < cctvsize; i++) {
+				int[] v = list.get(i);
+
+				roatatecnt(output[i], cctvcopy, arrcopy[v[0]][v[1]] - '0');
+				spread(v[0], v[1], arrcopy, cctvcopy);
 			}
 
-			// cctv번호와 순열로 뽑혀진 방향에 맞는 상하좌우 방향 설정
-			for (int i = 0; i < cctvList.size(); i++) {
-				direction(cctvList.get(i), output[i]);
+			res = Math.min(res, getcount(arrcopy));
+			// System.out.println("show");
+//			System.out.println("cctvsize" + cctvsize);
+
+			for (int i = 0; i < arr.length; i++) {
+				// System.out.println(Arrays.toString(arrcopy[i]));
 			}
-
-			// 사각 지대 구하기
-			getBlindSpot();
-
+			// System.out.println("getcount" + getcount(arrcopy));
 			return;
 		}
 
 		for (int i = 0; i < 4; i++) {
 			output[depth] = i;
-			permutation(depth + 1, r);
+			permu(depth + 1);
+
+		}
+
+	}
+	// 상좌하우
+
+	static void spread(int r, int c, char[][] arrcopy, int[][] cctvcopy) {
+		Queue<int[]> q = new LinkedList<>();
+
+		ArrayList<Integer> dir = new ArrayList<>();
+		// System.out.println("spread start");
+		// System.out.println("cctvcidx r c" + arrcopy[r][c] + " " + r + " " + c);
+
+		for (int i = 0; i < 4; i++) {
+//			System.out.println("      c" + Arrays.toString(cctvcopy[i]));
+		}
+
+		for (int i = 0; i < 4; i++) {
+			if (1 == cctvcopy[(arrcopy[r][c]) - '0' - 1][i]) {
+				dir.add(i);
+			}
+		}
+//
+		// System.out.println("dir " + (int) ((arrcopy[r][c]) - '0') + " " +
+		// dir.toString() + " r c " + r + " " + c);
+
+		for (int i = 0; i < dir.size(); i++) {
+			q.add(new int[] { r, c, dir.get(i) });
+		}
+
+		int size = q.size();
+
+		for (int i = 0; i < size; i++) {
+			int[] v = q.poll();
+			spread2(v[0], v[1], arrcopy, cctvcopy, v[2]);
+		}
+
+	}
+
+	static void spread2(int r, int c, char[][] arrcopy, int[][] cctvcopy, int dir) {
+		Queue<int[]> q = new LinkedList<>();
+
+		q.add(new int[] { r, c, dir });
+
+		while (!q.isEmpty()) {
+			int[] v = q.poll();
+//			System.out.println("v0 v1 v2 " + v[0] + " " + v[1] + " " + v[2]);
+			int nr = v[0] + dr[v[2]];
+			int nc = v[1] + dc[v[2]];
+
+			if (isvalid(nr, nc) && arrcopy[nr][nc] != '6') {
+				// System.out.println("nr nc" + nr + " " + nc);
+				q.add(new int[] { nr, nc, v[2] });
+				visit[nr][nc] += 1;
+
+				if (arrcopy[nr][nc] < '1' || arrcopy[nr][nc] > '5')
+					arrcopy[nr][nc] = '#';
+
+			}
+		}
+
+	}
+	// 상좌하우
+
+	static void respread(int r, int c, char[][] arrcopy, int[][] cctvcopy) {
+		Queue<int[]> q = new LinkedList<>();
+
+		ArrayList<Integer> dir = new ArrayList<>();
+		// System.out.println("respread start");
+		// System.out.println("cctvcidx " + arrcopy[r][c]);
+
+		for (int i = 0; i < 4; i++) {
+//			System.out.println("      c" + Arrays.toString(cctvcopy[i]));
+		}
+		for (int i = 0; i < 4; i++) {
+			if (1 == cctvcopy[(arrcopy[r][c]) - '0' - 1][i]) {
+				dir.add(i);
+			}
+		}
+		// System.out.println("dir " + (int) ((arrcopy[r][c]) - '0') + " " +
+		// dir.toString() + " r c " + r + " " + c);
+
+		for (int i = 0; i < dir.size(); i++) {
+
+			q.add(new int[] { r, c, dir.get(i) });
+		}
+
+		int size = q.size();
+
+		for (int i = 0; i < size; i++) {
+			int[] v = q.poll();
+			respread2(v[0], v[1], arrcopy, cctvcopy, v[2]);
+		}
+
+	}
+
+	static void respread2(int r, int c, char[][] arrcopy, int[][] cctvcopy, int dir) {
+		Queue<int[]> q = new LinkedList<>();
+
+		q.add(new int[] { r, c, dir });
+
+		while (!q.isEmpty()) {
+			int[] v = q.poll();
+//			System.out.println("v0 v1 v2 " + v[0] + " " + v[1] + " " + v[2]);
+			int nr = v[0] + dr[v[2]];
+			int nc = v[1] + dc[v[2]];
+
+			if (isvalid(nr, nc) && arrcopy[nr][nc] == '#') {
+				// System.out.println("nr nc" + nr + " " + nc);
+				q.add(new int[] { nr, nc, v[2] });
+				visit[nr][nc] -= 1;
+//				if (visit[nr][nc] <= 0)
+				if (visit[nr][nc] <= 0 && (arrcopy[nr][nc] < '1' || arrcopy[nr][nc] > '5'))
+					arrcopy[nr][nc] = '0';
+			}
+		}
+
+	}
+
+	static boolean isvalid(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < M;
+	}
+
+	static void rotate(int cctvidx, int[][] cctvcopy) {
+		int tmp;
+		cctvidx--;
+		// System.out.println("cctvidx" + cctvidx);
+		int size = cctvcopy[cctvidx].length;
+		tmp = cctvcopy[cctvidx][size - 1];
+
+		for (int j = size - 1; j > 0; j--) {
+			cctvcopy[cctvidx][j] = cctvcopy[cctvidx][j - 1];
+		}
+
+		cctvcopy[cctvidx][0] = tmp;
+	}
+
+	static void roatatecnt(int cnt, int[][] cctvcopy, int cctvidx) {
+		for (int i = 0; i < cnt; i++) {
+			rotate(cctvidx, cctvcopy);
 		}
 	}
 
-	// 각 cctv 번호와 순열로 뽑혀진 방향에 맞게 감시
-	public static void direction(CCTV cctv, int d) {
-		int cctvNum = cctv.num;
-
-		if (cctvNum == 1) {
-			if (d == 0)
-				spread(cctv, 0); // 상
-			else if (d == 1)
-				spread(cctv, 1); // 우
-			else if (d == 2)
-				spread(cctv, 2); // 하
-			else if (d == 3)
-				spread(cctv, 3); // 좌
-		} else if (cctvNum == 2) {
-			if (d == 0 || d == 2) {
-				spread(cctv, 0);
-				spread(cctv, 2); // 상하
-			} else {
-				spread(cctv, 1);
-				spread(cctv, 3); // 좌우
-			}
-		} else if (cctvNum == 3) {
-			if (d == 0) {
-				spread(cctv, 0); // 상우
-				spread(cctv, 1);
-			} else if (d == 1) {
-				spread(cctv, 1); // 우하
-				spread(cctv, 2);
-			} else if (d == 2) {
-				spread(cctv, 2); // 하좌
-				spread(cctv, 3);
-			} else if (d == 3) {
-				spread(cctv, 0); // 좌상
-				spread(cctv, 3);
-			}
-		} else if (cctvNum == 4) {
-			if (d == 0) {
-				spread(cctv, 0); // 좌상우
-				spread(cctv, 1);
-				spread(cctv, 3);
-			} else if (d == 1) {
-				spread(cctv, 0); // 상우하
-				spread(cctv, 1);
-				spread(cctv, 2);
-			} else if (d == 2) {
-				spread(cctv, 1); // 좌하우
-				spread(cctv, 2);
-				spread(cctv, 3);
-			} else if (d == 3) {
-				spread(cctv, 0); // 상좌하
-				spread(cctv, 2);
-				spread(cctv, 3);
-			}
-		} else if (cctvNum == 5) { // 상우하좌
-			spread(cctv, 0);
-			spread(cctv, 1);
-			spread(cctv, 2);
-			spread(cctv, 3);
-		}
-	}
-
-	// BFS로 방향에 맞게 감시
-	public static void spread(CCTV cctv, int d) {
-		Queue<CCTV> queue = new LinkedList<>();
-		boolean[][] visited = new boolean[N][M];
-
-		queue.add(cctv);
-		visited[cctv.x][cctv.y] = true;
-
-		while (!queue.isEmpty()) {
-			int nx = queue.peek().x + dx[d];
-			int ny = queue.poll().y + dy[d];
-
-			// 범위를 벗어나거나 벽을 만나면 끝
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M || copyMap[nx][ny] == 6) {
-				break;
-			}
-
-			if (copyMap[nx][ny] == 0) {
-				copyMap[nx][ny] = -1; // 빈칸이라면 감시할 수 있다는 의미로 -1
-				queue.add(new CCTV(cctv.num, nx, ny));
-			} else { // 다른 cctv가 있거나 이미 감시된 칸이라면
-				queue.add(new CCTV(cctv.num, nx, ny)); // 그냥 통과
-			}
-		}
-	}
-
-	// 사각 지대 구하기
-	public static void getBlindSpot() {
+	static int getcount(char[][] arrcopy) {
 		int cnt = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (copyMap[i][j] == 0) {
+				if (arrcopy[i][j] == '0')
 					cnt++;
-				}
 			}
 		}
-		answer = Math.min(answer, cnt);
+
+		return cnt;
 	}
 
-}
-
-class CCTV {
-	int num;
-	int x;
-	int y;
-
-	CCTV(int num, int x, int y) {
-		this.num = num;
-		this.x = x;
-		this.y = y;
-	}
 }
