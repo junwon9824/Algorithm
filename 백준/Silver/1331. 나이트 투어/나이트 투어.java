e8454ba05629b9b8167f7dc314e8d001c2e6894f
junwon9824 <42957005+junwon9824@users.noreply.github.com>
2023-09-18 20:43:57 +0900

    [Silver IV] Title: 나이트 투어, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1331. 나이트 투어/나이트 투어.java
+++ 백준/Silver/1331. 나이트 투어/나이트 투어.java
@@ -11,7 +11,7 @@ import javax.swing.InputMap;
 
 public class Main {
 	static int[] dr = { 2, 2, 1, 1, -1, -1, -2, -2 };
-	static int[] dc = { 1, -1, -2, 2, 2, -2, -1, 1 };
+	static int[] dc = { 1, -1, -2, 2, 2, -2, -1, 1 }; //나이트 이동 가능 
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -31,11 +31,11 @@ public class Main {
 		boolean[][] visit = new boolean[6][6];
 		char[] first = new char[2];
 		first[0] = str[0].charAt(0);
-		first[1] = str[0].charAt(1);
+		first[1] = str[0].charAt(1); // 나이트의 첫 번째위치
 
 		char[] last = new char[2];
 		last[0] = str[35].charAt(0);
-		last[1] = str[35].charAt(1);
+		last[1] = str[35].charAt(1);// 나이트의 마지막위치
 
 		for (int i = 0; i < 36; i++) {
 			boolean flag = false;
@@ -44,26 +44,27 @@ public class Main {
 
 			if (i >= 1) {
 				int r = str[i - 1].charAt(0) - ch[0];
-				int c = str[i - 1].charAt(1) - ch[1];
+				int c = str[i - 1].charAt(1) - ch[1];  //바로 전 본인의 위치와의 차이 
 
 				for (int j = 0; j < 8; j++) {
-					if (r == dr[j] && c == dc[j]) {
+					if (r == dr[j] && c == dc[j]) { // 만약 바로전과 비교하여 갈수 있는 위치인경우 true 
+                        //할당 
 						flag = true;
 					}
 
 				}
 
-				if (flag == false) {
+				if (flag == false) { //갈수 없는 위치였을경우 invalid 출력
 					System.out.println("Invalid");
 					return;
 				}
 			}
 
 			if (!visit[6 - (ch[1] - '0')][ch[0] - 'A'])
-				visit[6 - (ch[1] - '0')][ch[0] - 'A'] = true;
+				visit[6 - (ch[1] - '0')][ch[0] - 'A'] = true; //방문처리
 
 			else {
-				System.out.println("Invalid");
+				System.out.println("Invalid"); //방문한데 또 간경우는 안되므로 invalid
 				return;
 			}
 
@@ -72,11 +73,11 @@ public class Main {
 		
 		for (int j = 0; j < 8; j++) {
 			if (first[0] - last[0] == dr[j] && first[1] - last[1] == dc[j]) {
-				cnt++;
+				cnt++; //첫번째에서 마지막칸으로 이동 가능해야 하기때문에 이 로직을 짠것.
 			}
 		}
 
-		if (cnt == 0) {
+		if (cnt == 0) { //불가능한경우 invalid 
 			System.out.println("Invalid");
 			return;
 		}
