529a7235568228eb1c850ca8d574256ce267b2f9
junwon9824 <42957005+junwon9824@users.noreply.github.com>
2023-12-30 16:48:35 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT ao.ANIMAL_ID ,  ao.NAME
+
+from ANIMAL_INS ai   right join ANIMAL_OUTS ao on ai.animal_id = ao.animal_id 
+
+ where ai.ANIMAL_TYPE is null
+
+order by ao.ANIMAL_ID
