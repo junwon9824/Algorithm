cbba6545509134a475901fa7f9f032f9e27ee2d1
junwon9824 <42957005+junwon9824@users.noreply.github.com>
2024-03-08 00:42:13 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,6 +1,6 @@
 -- 코드를 작성해주세요
 
-select sum(g.score ) as SCORE,e.EMP_NO as EMP_NO,	EMP_NAME,	POSITION,	EMAIL
+select sum(g.score ) as SCORE,  e.EMP_NO as EMP_NO,	EMP_NAME,	POSITION,	EMAIL
 
 from HR_GRADE g join  HR_EMPLOYEES e on g.EMP_NO=e.EMP_NO
 where  g.year='2022'
@@ -8,4 +8,3 @@ where  g.year='2022'
 group by e.EMP_NO 
 order by  SCORE desc
 limit 1
-
