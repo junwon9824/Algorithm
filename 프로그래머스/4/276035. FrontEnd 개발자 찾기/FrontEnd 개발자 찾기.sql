3c659b80c1d1567fbdab20791f5c81f501ff8b93
junwon9824 <42957005+junwon9824@users.noreply.github.com>
2024-03-09 21:39:50 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,13 @@
+-- 코드를 작성해주세요
+select ID	
+,EMAIL
+,FIRST_NAME	
+,LAST_NAME
+
+
+from
+DEVELOPERS  d
+
+where SKILL_CODE &(select sum(code) code from SKILLCODES where category='Front End')>0 
+
+order by ID asc
