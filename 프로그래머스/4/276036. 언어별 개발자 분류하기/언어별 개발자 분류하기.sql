85ddff4747538f3105a9b3b65e79117065792b09
junwon9824 <42957005+junwon9824@users.noreply.github.com>
2024-03-09 21:19:46 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -0,0 +1,41 @@
+-- 코드를 작성해주세요
+with f as (
+select sum(CODE)   code
+from
+SKILLCODES 
+where
+category='Front End'
+)
+
+select
+
+ case
+ 
+ when
+d.SKILL_CODE & (select CODE from f ) and 
+d.SKILL_CODE & (select CODE from SKILLCODES where NAME='Python')  
+ then 'A'
+ 
+ when
+d.SKILL_CODE & (select CODE from SKILLCODES where NAME='C#' )
+ then 'B'
+ 
+ 
+ when
+d.SKILL_CODE & (select CODE from f  )
+ then 'C'
+  
+  
+ end
+ 
+ as GRADE , 
+ ID	 ,
+ EMAIL
+
+from
+DEVELOPERS d
+
+HAVING
+  GRADE IS NOT NULL
+
+order by GRADE , ID asc
