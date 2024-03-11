e15540b79b1cb33af4199f4b70aa095d7543ae93
junwon9824 <42957005+junwon9824@users.noreply.github.com>
2024-03-11 23:13:05 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -3,6 +3,6 @@ SELECT ao.ANIMAL_ID ,  ao.NAME
 
 from ANIMAL_INS ai   right join ANIMAL_OUTS ao on ai.animal_id = ao.animal_id 
 
- where ai.ANIMAL_TYPE is null
+ where ai.SEX_UPON_INTAKE is null
 
 order by ao.ANIMAL_ID
