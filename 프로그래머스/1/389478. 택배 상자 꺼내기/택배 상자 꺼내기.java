d2d98e4300825361a7a3a7a43921d4bd6ba6979f
junwon9824 <42957005+junwon9824@users.noreply.github.com>
2025-05-08 16:48:10 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 14.53 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,17 +1,13 @@
 class Solution {
     public int solution(int n, int w, int num) {
         
-        // n=5;
-        // w=2;
-        // num=2;
-        
         int layer = (num - 1) / w;
             
         int idx = (num - 1) % w;
 
         // num의 실제 열 위치 구하기 (지그재그 형태 반영)
         int col;
-        System.out.println("idx "+idx);
+        System.out.println(idx);
         
         if (layer % 2 == 0) {
             col = idx;
@@ -19,29 +15,18 @@ class Solution {
             col = w - 1 - idx;
         }
 
-        
-        
         int count = 0;
         
          int h = (n + w - 1) / w; // 전체 층 수
-        
-        System.out.println("col "+col);        
-        // 5   
-        // 43
-        // 12
-        
+
         // 위층부터 아래층까지 같은 열에 있는 상자 세기
         for (int i = layer; i < h; i++) {
-            
             int boxNum;
-            
             if (i % 2 == 0) {
                 boxNum = i * w + col + 1;
             } else {
-                boxNum = i * w + (w  - col) ;
+                boxNum = i * w + (w - 1 - col) + 1;
             }
-            
-            System.out.println("i "+i+" booxnum " +boxNum );
 
             if (boxNum <= n) count++;
         }
