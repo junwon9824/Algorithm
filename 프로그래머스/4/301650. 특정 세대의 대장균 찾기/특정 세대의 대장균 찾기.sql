44327369a1233873d0a87654240d5ef095490833
junwon9824 <42957005+junwon9824@users.noreply.github.com>
2025-08-26 16:52:49 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,20 +1,37 @@
--- 코드를 작성해주세요
+# WITH recursive recurss AS (
+#     -- 1세대: PARENT_ID가 NULL인 개체들
+#     SELECT ID, PARENT_ID, 1 AS generation
+#     FROM ECOLI_DATA
+#     WHERE PARENT_ID IS NULL
+    
+#     UNION 
+    
+#     -- 자식 개체들을 재귀적으로 찾기
+#     SELECT e.ID, e.PARENT_ID, g.generation + 1 as generation
+#     FROM ECOLI_DATA e
+#     JOIN recurss g ON e.PARENT_ID = g.ID
+# )
 
-select id 
+# # -- 3세대 개체들만 선택
+# SELECT id
+# FROM recurss
+# WHERE generation = 3
+# ORDER BY ID;
+
+with recursive rc as (
+select   1 as generation , id
 from ECOLI_DATA
-
-where parent_id in 
-(
-select id 
-from ECOLI_DATA
-
-where parent_id in (
-select ID
-
-from ECOLI_DATA 
-
-where parent_id is null
+where PARENT_ID is null
+    
+union 
+select rc.generation+1, e.id
+from ECOLI_DATA e  join  rc on 
+rc.id=e.PARENT_ID 
+    
 )
-    )
 
-order by ID asc
+SELECT id
+FROM rc
+WHERE generation = 3
+ORDER BY id;
+
